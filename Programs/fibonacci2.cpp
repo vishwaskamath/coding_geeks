ba550e2dde8b7145786d718ab797e207859787da
M VISHWAS KAMATH <vishwask.cs18@sahyadri.edu.in>
2020-05-20 09:29:00 +0530

    Create fibonacci2.cpp

---
+++ Programs/fibonacci2.cpp
@@ -0,0 +1,28 @@
+//Fibonacci Series using Dynamic Programming 
+#include<bits/stdc++.h>
+using namespace std; 
+
+int fib(int n) 
+{ 
+
+int f[n+2]; 
+int i; 
+
+f[0] = 0; 
+f[1] = 1; 
+
+for (i = 2; i <= n; i++) 
+{ 
+	f[i] = f[i-1] + f[i-2]; 
+} 
+
+return f[n]; 
+} 
+
+int main () 
+{ 
+int n;
+cin>>n; 
+cout<<fib(n);
+return 0; 
+} 

2d6d5ff90c029264d91a30185326f6894c842241
M VISHWAS KAMATH <vishwask.cs18@sahyadri.edu.in>
2020-06-06 13:21:13 +0530

    Create pattern01.cpp

---
+++ Programs/pattern01.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+  int n,b,c;
+  cout<<"Enter the number of lines "<<endl;
+  cin>>n;
+  cout<<"Your patter is as follows "<<endl;
+  for(int i =1; i<=n; i++)
+  {
+  for(int j =1; j<=i;j++)
+    {
+     cout<<"*";
+    }
+  cout<<endl;
+  }
+  
+}

6c249b3bfe800a420e4b2b23e591f060c58f6b19
M VISHWAS KAMATH <vishwask.cs18@sahyadri.edu.in>
2020-06-06 13:21:41 +0530

    Update pattern01.cpp

--- Programs/pattern01.cpp
+++ Programs/pattern01.cpp
@@ -2,13 +2,13 @@
 using namespace std;
 int main()
 {
-  int n,b,c;
+  int n,i,j;
   cout<<"Enter the number of lines "<<endl;
   cin>>n;
   cout<<"Your patter is as follows "<<endl;
-  for(int i =1; i<=n; i++)
+  for(i =1; i<=n; i++)
   {
-  for(int j =1; j<=i;j++)
+  for(j =1; j<=i;j++)
     {
      cout<<"*";
     }
